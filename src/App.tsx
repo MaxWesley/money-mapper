acb26edd4a351707228d206e9c79fa1ed2688fce
MaxWesley <mx_wesley@hotmail.com>
2023-01-06 23:09:13 -0300

    feat: create input extract

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-
 function App() {
   return (
     <div className="App">
       <h2>Money Mapper</h2>
+      <label htmlFor="extract-input">
+        <p>Importe seus extratos aqui</p>
+        <input type="file" name="extract-input" id="extract-input" />
+      </label>
     </div>
   );
 }
